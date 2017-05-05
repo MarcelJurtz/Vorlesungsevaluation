dfdc64bf80104d57577aa0ac9903854665ae014b
MarcelJurtz <jurtzmarcel@gmail.com>
2017-05-05 10:30:26 +0200

    Add admin functions

---
+++ functions-admin.inc.php
@@ -0,0 +1,36 @@
+<?php
+function changePassword($oldPassword, $newPassword, $newPasswordConfirmed) {
+  $conn = getDBConnection();
+
+  $oldPassword = md5(mysqli_real_escape_string($conn,$oldPassword));
+  $newPassword = md5(mysqli_real_escape_string($conn,$newPassword));
+  $newPasswordConfirmed = md5(mysqli_real_escape_string($conn,$newPasswordConfirmed));
+
+  if($oldPassword == NULL || $oldPassword == '' ||
+      $newPasswordConfirmed == NULL || $newPasswordConfirmed == '' ||
+      $newPassword == NULL || $newPassword == '') {
+        echo 'Mindestens eine der Eingaben fehlt. Bitte überpüfen Sie diese.';
+  } else if($newPassword != $newPasswordConfirmed) {
+        echo 'Die Passwörter stimmen nicht überein.';
+  } else {
+
+    $query = "SELECT " . ADMINISTRATOR_AKennwort . " FROM " . ADMINISTRATOR . " WHERE " . ADMINISTRATOR_AName . " = '" . ADMIN_DEFAULT_USERNAME . "';";
+    $getAdminPW = mysqli_fetch_assoc(mysqli_query($conn,$query));
+    $adminPW = $getAdminPW[ADMINISTRATOR_AKennwort];
+
+    if($adminPW == $oldPassword) {
+      $query = "UPDATE " . ADMINISTRATOR . " SET " . ADMINISTRATOR_AKennwort . " = '$newPassword' WHERE " . ADMINISTRATOR_AName . " = '" . ADMIN_DEFAULT_USERNAME . "';";
+      echo $query;
+      if(mysqli_query($conn,$query)) {
+        echo 'Änderung erfolgreich durchgeführt.';
+      } else {
+        echo 'Fehler beim Speichern der neuen Anmeldedaten.';
+      }
+    } else {
+      echo 'Falsches Passwort.';
+    }
+
+    mysqli_close($conn);
+  }
+}
+?>
