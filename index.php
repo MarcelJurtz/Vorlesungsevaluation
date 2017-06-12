82cc697207b9c3d6c3d0cae88eea85206e5d88d4
MarcelJurtz <jurtzmarcel@gmail.com>
2017-06-12 10:39:24 +0200

    fix redirection url

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	//header('Location: '.$uri.'/projekt/student/index.html');
-	header('Location: '.'./student/index.html');
+	header('Location: '.'./student/index.php');
 	exit;
 ?>
